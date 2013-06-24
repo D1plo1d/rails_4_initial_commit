2bc89189ad6b2945cd6e0584456624287c225f12
D1plo1d <d1plo1d@d1plo1d.com>
2013-06-24 16:39:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitmakerCms::Application.config.secret_key_base = '1200ca95b46afdd6fbc8fdc7494dec1d0d7bd5430785865db59a21c57bab8b065e857d84791a133363063903f5a29d4b71b0d5a2554d46d7cbf9c2db8c341476'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BitmakerCms::Application.config.session_store :cookie_store, key: '_bitmaker_cms_session'
